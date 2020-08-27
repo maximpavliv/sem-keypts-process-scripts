116afc37ed3002f94f1bfe01c5a04dc011ded347
maximpavliv <maxim.pavliv@epfl.ch>
2020-08-27 12:49:20 +0200

    changed some stuff to produce annot file for whole valid set

--- create_mat_annot.m
+++ create_mat_annot.m
@@ -5,21 +5,23 @@ data_names = textscan(names_file,'%s');
 fclose(names_file);
 names_full=data_names{1};
 
-nb_samples = 20
+%nb_samples = 20
+nb_samples = size(names_full);
 
 names_full_nojpg = cellfun(@(x) x(1:5),names_full,'un',0);
 
 A = [565.2027524412007, 0, 492.48969368881677; 0, 565.6613573390968, 271.0003077726479; 0, 0, 1];
 intrinsics = {};
 for i = [1:nb_samples]
-    intrinsics = [intrinsics, A]
+    intrinsics = [intrinsics, A];
 end
 
-annot = struct()
-annot.imgname = names_full(1:nb_samples)
-annot.center = permute(valid_full_centers(:,1:nb_samples),[2 1])
-annot.scale = valid_full_scales(1:nb_samples)
-annot.K = intrinsics
+annot = struct();
+%annot.imgname = names_full(1:nb_samples)
+annot.imgname = names_full_nojpg(1:nb_samples);
+annot.center = permute(valid_full_centers(:,1:nb_samples),[2 1]);
+annot.scale = valid_full_scales(1:nb_samples);
+annot.K = intrinsics;
 
 
 save valid annot
